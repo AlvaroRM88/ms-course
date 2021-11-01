e700a11428b62e4bfa0a68592e5826f980aa78e0
AlvaroRM88 <arm071088@gmail.com>
2021-11-01 12:33:21 +0100

    step_10_eureka_2_clients_config

--- service-cart/src/main/java/com/stratesys/cart/feign/ServiceProductFeign.java
+++ service-cart/src/main/java/com/stratesys/cart/feign/ServiceProductFeign.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.stratesys.cart.dtos.ProductDto;
 
-@FeignClient(name = "service-product", url="localhost:8001/product")
+@FeignClient(name = "service-product")
 public interface ServiceProductFeign {
 	
-	@GetMapping("/all")
+	@GetMapping("/product/all")
 	public List<ProductDto> findAll();
 	
-	@GetMapping("/by/{id}")
+	@GetMapping("/product/by/{id}")
 	public ProductDto findById(@PathVariable Long id);
 
 }
