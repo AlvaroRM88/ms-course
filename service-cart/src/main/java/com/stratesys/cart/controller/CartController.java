49a3fec35ea31d832bf83a88670876356e92e022
AlvaroRM88 <arm071088@gmail.com>
2021-11-01 14:19:43 +0100

    step_15_test_with_cart_controller

--- service-cart/src/main/java/com/stratesys/cart/controller/CartController.java
+++ service-cart/src/main/java/com/stratesys/cart/controller/CartController.java
@@ -1,9 +1,16 @@
 package com.stratesys.cart.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +22,11 @@ import com.stratesys.cart.services.CartService;
 @RestController
 @RequestMapping("/cart")
 public class CartController {
+	
+	private static Logger log = LoggerFactory.getLogger(CartController.class);
+
+	@Value("${configuracion.prueba}")
+	private String test;
 
 	@Autowired
 	@Qualifier("serviceFeign")
@@ -29,4 +41,18 @@ public class CartController {
 	public CartDto findById(@PathVariable Long id, @PathVariable Integer amount) {
 		return cartService.findById(id, amount);
 	}
+	
+	
+	@GetMapping("/config")
+	public ResponseEntity<?> getConfig(@Value("${server.port}") String port){
+		
+		log.info(test);
+		
+		Map<String, String> json = new HashMap<>();
+		json.put("config_test", test);
+		json.put("port", port);
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
+
 }
